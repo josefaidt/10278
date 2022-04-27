76b348862f30ae10cdb7c12ed7c8568a473d5de5
josefaidt <josef.aidt@gmail.com>
2022-04-27 12:10:53 -0500

    🚀

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,27 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "10278a9062958": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "function": {
+        "S3Trigger7008eb43": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "s34fb1bc17": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
